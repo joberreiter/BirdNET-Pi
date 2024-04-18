739272259e5da7799f03892587f5cd517048e399
frederik <frederik.slos@gmail.com>
2024-04-18 14:37:05 +0200

    en.php

---
+++ homepage/locale/en.php
@@ -0,0 +1,42 @@
+<?php
+
+global $langArray;
+$langArray = array(
+  "best_recordings" => "Best Recordings",
+  "currently_analyzing" => "Currently Analyzing",
+  "custom_species_list" => "Custom Species List",
+  "daily_charts" => "Daily Chart",
+  "database_maintenance" => "Database Maintenance",
+  "excluded_species_list" => "Excluded Species List",
+  "file_manager" => "File Manager",
+  "five_most_recent" => "5 Most Recent Detections",
+  "last_hour" => "Last Hour",
+  "no_detections_for_today" => "No Detections For Today",
+  "number_of_detections" => "Number of Detections",
+  "overview" => "Overview",
+  "recordings" => "Recordings",
+  "search_" => "Search...",
+  "services" => "Services",
+  "settings" => "Settings",
+  "species_detected_today" => "Species Detected Today",
+  "species_stats" => "Species Stats",
+  "spectrogram" => "Spectrogram",
+  "system_controls" => "System Controls",
+  "system_info" => "System Info",
+  "today" => "Today",
+  "todays_detections" => "Today's Detections",
+  "tools" => "Tools",
+  "total" => "Total",
+  "total_number_of_species" => "Total Number of Species",
+  "unique_species_today" => "Unique Species Today",
+  "unique_species_total" => "Unique Species Total",
+  "view_log" => "View Log",
+  "web_terminal" => "Web Terminal",
+  
+  "zzz" => "Today",
+);
+
+//return $langArray;
+
+// .$langArray['welcome'].
+//<?php echo $langArray['welcome']; ?>
