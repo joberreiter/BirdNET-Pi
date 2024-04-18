cf37e0b781ec381c5c0a07a75880702c89effa2e
frederik <frederik.slos@gmail.com>
2024-04-18 14:38:45 +0200

    nl.php

---
+++ homepage/locale/nl.php
@@ -0,0 +1,41 @@
+<?php
+
+global $langArray;
+$langArray = array(
+  "best_recordings" => "Beste Opnames",
+  "currently_analyzing" => "Momenteel aan het Analyseren",
+  "custom_species_list" => "Aangepaste Soortenlijst",
+  "daily_charts" => "Dagelijkse Grafiek",
+  "database_maintenance" => "Databaseonderhoud",
+  "excluded_species_list" => "Uitgesloten Soortenlijst",
+  "file_manager" => "Bestandsbeheer",
+  "five_most_recent" => "De 5 Meest Recente Detecties",
+  "last_hour" => "Laatste uur",
+  "no_detections_for_today" => "Geen Detecties Vandaag",
+  "number_of_detections" => "Aantal Detecties",
+  "overview" => "Overzicht",
+  "recordings" => "Opnames",
+  "search_" => "Zoeken...",
+  "services" => "Services",
+  "settings" => "Instellingen",
+  "species_detected_today" => "Vandaag Gedetecteerde Soorten",
+  "species_stats" => "Soortstatistieken",
+  "spectrogram" => "Spectrogram",
+  "system_controls" => "System Controls",
+  "system_info" => "Systeem Info",
+  "today" => "Vandaag",
+  "todays_detections" => "Detecties Vandaag",
+  "tools" => "Gereedschap",
+  "total" => "Totaal",
+  "total_number_of_species" => "Totaal Aantal Soorten",
+  "unique_species_today" => "Unieke Soorten Vandaag",
+  "unique_species_total" => "Unieke Soorten Totaal",
+  "view_log" => "Bekijk Log",
+  "web_terminal" => "Webterminal",
+  
+  "zzz" => "zzz",
+);
+
+
+// .$langArray['welcome'].
+//<?php echo $langArray['welcome']; ?>
